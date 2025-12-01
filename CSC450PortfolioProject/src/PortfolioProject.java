a8ba49a0f4a6ef56a79a91b83f3f880bc1436188
adamgorry <adamgorry@mac-923.lan>
2025-11-30 18:23:55 -0700

    Created and joined CountingUpThread and CountingDownThread

--- CSC450PortfolioProject/src/PortfolioProject.java
+++ CSC450PortfolioProject/src/PortfolioProject.java
@@ -29,10 +29,31 @@ public class PortfolioProject {
 	}
 	
 	
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("Let the counting begin ");  //Intro message
+		
+		CountingUpThread thread1 = new CountingUpThread();  //Create and start the first thread
+		thread1.start();
+		
+		try {  //Main thread waits for thread1 to finish counting up
+			thread1.join();
+		} catch (InterruptedException oops) {
+			System.out.println("Main thread interrupted.");
+		}
+		
+		System.out.println("Counting down");
+		
+		CountingDownThread thread2 = new CountingDownThread();  //Create and start the second thread
+		thread2.start();
+		
+		try {  //Main thread waits for thread2 to finish counting down
+			thread2.join();
+		} catch (InterruptedException oops) {
+			System.out.println("Main thread interrupted.");
+		}
+		
+		System.out.println("Both threads have completed their counting.");
 	}
-
 }
