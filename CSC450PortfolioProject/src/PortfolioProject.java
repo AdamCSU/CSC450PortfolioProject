17f3e10e5c15cd8d7d0e56d768c03b525a8985c7
adamgorry <adamgorry@mac-923.lan>
2025-11-30 18:00:25 -0700

    Created CountingUp and CountingDown thread classes

--- CSC450PortfolioProject/src/PortfolioProject.java
+++ CSC450PortfolioProject/src/PortfolioProject.java
@@ -1,5 +1,34 @@
-
 public class PortfolioProject {
+	
+	
+	static class CountingUpThread extends Thread {  //Thread class for counting up
+		public void run() {
+			for (int i = 1; i <= 20; i++) {
+				System.out.println("Counting up: " + i);
+				try {
+					Thread.sleep(175);
+				} catch (InterruptedException oops) {
+					System.out.println("CountingUpThread interrupted.");
+				}
+			}
+		}
+	}
+	
+	
+	static class CountingDownThread extends Thread {  //Thread class for counting down
+		public void run() {
+			for (int i = 20; i >= 0; i--) {
+				System.out.println("Counting down: " + i);
+				try {
+					Thread.sleep(175);
+				} catch (InterruptedException oops) {
+					System.out.println("CountingDownThread interrupted.");
+				}
+			}
+		}
+	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
